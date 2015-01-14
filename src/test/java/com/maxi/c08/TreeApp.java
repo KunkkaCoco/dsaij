81cf05ca24e4c06f9bfdb3c1b1908df6bdc9d862
chenweichao <chenweichao@news.cn>
2015-01-14 17:54:15 +0800

    TEST IDEA

--- src/test/java/com/maxi/c08/TreeApp.java
+++ src/test/java/com/maxi/c08/TreeApp.java
@@ -5,39 +5,39 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TreeApp {
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		System.out.println("before test##############################################################");
-	}
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        System.out.println("before test##############################################################");
+    }
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		System.out.println("AfterClass test##########################################################");
-	}
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        System.out.println("AfterClass test##########################################################");
+    }
 
-	@Test
-	public void test() throws Exception {
-		int value;
-		Tree theTree = new Tree();
+    @Test
+    public void test() throws Exception {
+        int value;
+        Tree theTree = new Tree();
 
-		theTree.insert(50, 1.5);
-		theTree.insert(25, 1.2);
-		theTree.insert(75, 1.7);
-		theTree.insert(12, 1.5);
-		theTree.insert(37, 1.2);
-		theTree.insert(43, 1.7);
-		theTree.insert(30, 1.5);
-		theTree.insert(33, 1.2);
-		theTree.insert(87, 1.7);
-		theTree.insert(93, 1.5);
-		theTree.insert(97, 1.5);
+        theTree.insert(50, 1.5);
+        theTree.insert(25, 1.2);
+        theTree.insert(75, 1.7);
+        theTree.insert(12, 1.5);
+        theTree.insert(37, 1.2);
+        theTree.insert(43, 1.7);
+        theTree.insert(30, 1.5);
+        theTree.insert(33, 1.2);
+        theTree.insert(87, 1.7);
+        theTree.insert(93, 1.5);
+        theTree.insert(97, 1.5);
 
-		theTree.traverse(1);
-		theTree.traverse(2);
-		theTree.traverse(3);
+        theTree.traverse(1);
+        theTree.traverse(2);
+        theTree.traverse(3);
 
-		System.out.println(theTree.find(33));
-		theTree.displayTree();
+        System.out.println(theTree.find(33));
+        theTree.displayTree();
 
-	}
+    }
 }
